fe67efb6cac38168d619431d9692d744051fd2d1
hananOanunu <40333166+hananOanunu@users.noreply.github.com>
2023-06-11 18:23:54 +0300

    Instructions

--- Name.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// when the component receives true for the 'clicked' prop, add "(DEAD)" to the end of the name after 3 seconds.
-
-export default ({ name, clicked, ...restProps }) => {
-  return <div className={clicked ? 'clicked' : ''} {...restProps}>{name}</div>;
-}
-  
--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import { render } from 'react-dom';
 import Name from './Name';
 import './style.css';
 
-// get list of names from https://jsonplaceholder.typicode.com/users, render their names in a list using the Name component.
-// on a click on each name, it should change the 'clicked' prop of the component from false to true.
+/*
+   Your challenge should you choose to accept it:
+   1. Fetch a list of users from https://jsonplaceholder.typicode.com/users.
+   2. Display the users ID, Name and Email, as a table, where each raw represent a single user
+   3. The users should be displayed ordered by ID.
+   4. It should be possible to delete a user.
+   5. It should be possible to reorder the users, by dragging and dropping a user to the appropriate position. When a user possition changes, the id of the user should be updated as well to reflect the current position. For example, dragging user #5 to the first position, will update this user ID to be #1, the original first user  ID will become #2, the original second user Id will become #3 and so on. 
+*/
 
 const App = () => {
   return <div>{/*TODO*/}</div>;
